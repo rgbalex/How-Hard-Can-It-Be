41e31a0346a1c3b961c15a0db02ed54d5e903c60
beep-boop-boop <ivanbar3330@gmail.com>
2022-03-11 10:04:06 +0000

    Set up testing environment
    Edited build.gradle, settings.gradle and added build.gradle for the 'tests' module

---
+++ tests/src/com/mygdx/tests/AssetTests.java
@@ -0,0 +1,4 @@
+package com.mygdx.tests;
+
+public class AssetTests {
+}
---
+++ tests/src/com/mygdx/tests/MovementTests.java
@@ -0,0 +1,9 @@
+package com.mygdx.tests;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+public class MovementTests {
+    @Test
+    public void simpleTest(){
+        assertTrue("testing junit", true);
+    }
+}
